a6e2f80f49a09ef8e2bfc48028ddeb831eacf0ce
Wenhan-Zhang-Reliabiliy <811475317@qq.com>
2025-07-09 14:34:43 +0800

    Update RITO.R
    I have modified the RITO to dynamically optimize the inspection time in the thermal cycling ALT. Here are the main functions for the RITO.

--- RITO.R
+++ RITO.R
@@ -156,7 +156,7 @@ vol_trace_simulation=function(theta,x,alpha,num_in_group,num_of_inspection){
   ### Comparision with fixed-time design
   fix_inspection=matrix(rep(500*(1:num_of_inspection),3),3,num_of_inspection,byrow = T)
   fix_num_matrix=num_in_inspection(failure_time,fix_inspection)
-  para_fix=optim(c(3.5,20,-2),Weibull_likelihood_interval,num_of_failure=fix_num_matrix,check_time=fix_inspection,method = "BFGS")$par
+  para_fix=optim(c(3.5,20,-2),Weibull_likelihood_interval,num_of_failure=fix_num_matrix,check_time=fix_inspection,method = "BFGS",hessian = T)$par
   para_est=optim(c(3.5,20,-2),Weibull_likelihood_interval,num_of_failure=num_matrix_initial,check_time=inspection_initial,method = "BFGS")$par
   theta_est=exp(para_est[2]+para_est[3]*x)
   inspection_time=rep(0,num_of_inspection)
@@ -254,3 +254,259 @@ DET=function(t,theta,alpha,x){
   }
   return(det(information))
 }
+
+##### Coffin-Manson-Arrhenius Model (Multi-stress)
+alpha=4
+deltaT=c(100,100,120,120)
+Tmax=c(373,393,373,393)
+K=8.623e-05
+b=1.9
+Ea=0.17
+A=10^4
+theta_CMA=A*deltaT^(-1.9)*exp(Ea/(K*Tmax))
+Ni=10
+num_in_group=10
+
+TR_CMA=function(t,theta,alpha,deltaT,Tmax){
+  n=length(t)
+  F=0
+  p=c()
+  ###interval
+  P=c()
+  for (i in 1:n) {
+    p=c(p,pweibull(t[i],alpha,theta)-F)
+    F=pweibull(t[i],alpha,theta)
+    P=c(P,F)
+  }
+  p=c(p,1-F)
+  Ai=alpha*(1-P)*log(1-P)
+  Ndp=Ni/p
+  coeff=c((1+log(deltaT)^2+(1/(K*Tmax))^2+1/(alpha^2)*log(t[1]/theta)^2)*Ai[1]^2)
+  for (i in 2:n) {
+    coe=(1+log(deltaT)^2)*(Ai[i]-Ai[i-1])^2+1/(alpha^2)*(log(t[i]/theta)*Ai[i]-log(t[i-1]/theta)*Ai[i-1])^2
+    coe1=(1+(1/(K*Tmax))^2)*(Ai[i]-Ai[i-1])^2+1/(alpha^2)*(log(t[i]/theta)*Ai[i]-log(t[i-1]/theta)*Ai[i-1])^2
+    coeff=c(coeff,coe+coe1)
+  }
+  coeff=c(coeff,(1+log(deltaT)^2+(1/(K*Tmax))^2+1/(alpha^2)*log(t[i]/theta)^2)*Ai[i]^2)
+  trace=sum(coeff*Ndp)
+  return(-trace)
+}
+
+TR_CMA_conditional=function(t_past,t_fu,theta,alpha,deltaT,Tmax){
+  n=length(t_past)+length(t_fu)
+  t=c(t_past,t_fu)
+  F=0
+  p=c()
+  ###interval
+  P=c()
+  for (i in 1:n) {
+    p=c(p,pweibull(t[i],alpha,theta)-F)
+    F=pweibull(t[i],alpha,theta)
+    P=c(P,F)
+  }
+  p=c(p,1-F)
+  Ai=alpha*(1-P)*log(1-P)
+  Ndp=Ni/p
+  coeff=c((1+log(deltaT)^2+(1/(K*Tmax))^2+1/(alpha^2)*log(t[1]/theta)^2)*Ai[1]^2)
+  for (i in 2:n) {
+    coe=(1+log(deltaT)^2)*(Ai[i]-Ai[i-1])^2+1/(alpha^2)*(log(t[i]/theta)*Ai[i]-log(t[i-1]/theta)*Ai[i-1])^2
+    coe1=(1+(1/(K*Tmax))^2)*(Ai[i]-Ai[i-1])^2+1/(alpha^2)*(log(t[i]/theta)*Ai[i]-log(t[i-1]/theta)*Ai[i-1])^2
+    coeff=c(coeff,coe+coe1)
+  }
+  coeff=c(coeff,(1+log(deltaT)^2+(1/(K*Tmax))^2+1/(alpha^2)*log(t[i]/theta)^2)*Ai[i]^2)
+  trace=sum(coeff*Ndp)
+  return(-trace)
+}
+
+###Varify the effectiveness of TR_CMA
+optim(c(300,400,500), TR_CMA_conditional,t_past=c(100,200),theta=theta_CMA[1],alpha=4,deltaT=deltaT[1],Tmax=Tmax[1],method = "BFGS")
+###$par [1] 306.5916 369.3287 429.1506
+log_deltaT=log(deltaT)
+inverse_KTmax=1/(K*Tmax)
+
+
+### Likelihood function
+Weibull_likelihood_interval_CMA=function(parameter,num_of_failure,check_time){
+  alpha=parameter[1]
+  A=exp(parameter[2])
+  b=parameter[3]
+  Ea=parameter[4]
+  theta=A*deltaT^(-b)*exp(Ea/(K*Tmax))
+  m=dim(num_of_failure)[1]
+  n=dim(num_of_failure)[2]
+  p=matrix(0,m,n)
+  for (i in 1:m) {
+    F=0
+    for (j in 1:(n-1)) {
+      p[i,j]=pweibull(check_time[i,j],alpha,theta[i])-F
+      F=pweibull(check_time[i,j],alpha,theta[i])
+    }
+    p[i,n]=1-F
+  }
+  logL=0
+  for (i in 1:m) {
+    logL=logL+sum(num_of_failure[i,]*log(p[i,]))
+  }
+  return(-logL)
+}
+
+check_time=matrix(c(100,150,300,100,150,300,100,150,300,100,150,300),4,3,byrow=T)
+num_of_failure=matrix(c(0,1,10,9,1,2,15,2,1,3,15,1,2,7,11,0),4,4,byrow=T)
+optim(c(3.5,9,1.9,0.17),Weibull_likelihood_interval_CMA,num_of_failure=num_of_failure,check_time=check_time,method = "BFGS")
+
+### Optimize the unconstrained trace
+optim(c(100,150,200,300,400,500), TR_CMA,theta=theta_CMA[1],alpha=4,deltaT=deltaT[1],Tmax=Tmax[1],method = "BFGS")
+### [1] 252.9793 307.0770 347.9708 384.7369 421.9655 465.8053
+### [1] -150036.9
+
+optim(c(120,150,200,300,400,450), TR_CMA,theta=theta_CMA[2],alpha=4,deltaT=deltaT[2],Tmax=Tmax[2],method = "BFGS")
+### [1] 193.3036 234.6562 265.9095 294.0111 322.4677 355.9813
+### [1] -137330.4
+
+optim(c(120,150,200,300,400,450), TR_CMA,theta=theta_CMA[3],alpha=4,deltaT=deltaT[3],Tmax=Tmax[3],method = "BFGS")
+### [1] 178.9093 217.1698 246.0910 272.0933 298.4242 329.4292
+### [1] -152329.7
+
+optim(c(120,150,200,250,300,310), TR_CMA,theta=theta_CMA[4],alpha=4,deltaT=deltaT[4],Tmax=Tmax[4],method = "BFGS")
+### [1] 136.7086 165.9571 188.0573 207.9310 228.0569 251.7520
+### [1] -137593.6
+###### Sum=577290.6
+
+### Fix-time design
+ 
+ TR_CMA(c(100,150,200,250,300,350),theta_CMA[1],4,deltaT[1],Tmax[1])
+ ### [1] -122960.9
+ TR_CMA(c(100,150,200,250,300,350),theta_CMA[2],4,deltaT[2],Tmax[2])
+ ### [1] -132262
+ TR_CMA(c(100,150,200,250,300,350),theta_CMA[3],4,deltaT[3],Tmax[3])
+ ### [1] -145664.4
+ TR_CMA(c(100,150,200,250,300,350),theta_CMA[4],4,deltaT[4],Tmax[4])
+ ### [1] -124633.9
+###### Sum=525521.2
+###### Relative efficiency=525521.2/577290.6=0.9103235
+
+CMA_Failure_times=function(theta,alpha,num_in_group){
+  failure_time=matrix(0,4,num_in_group)
+  for (i in 1:4) {
+    failure_time[i,]=sort(rweibull(num_in_group,alpha,theta[i]))
+  }
+  return(failure_time)
+}
+
+num_in_inspection_CMA=function(failure_time,inspection_time){
+  n=dim(inspection_time)[2]
+  num_matrix=matrix(0,4,n+1)
+  for (i in 1:4) {
+    num_matrix[i,1]=sum(failure_time[i,]<=inspection_time[i,1])
+    for (j in 2:n) {
+      num_matrix[i,j]=sum((failure_time[i,]>inspection_time[i,j-1])&(failure_time[i,]<=inspection_time[i,j]))
+    }
+    num_matrix[i,j+1]=sum(failure_time[i,]>inspection_time[i,j])
+  }
+  return(num_matrix)
+}
+
+CMA_trace_simulation=function(theta,alpha,deltaT,Tmax,num_in_group,num_of_inspection){
+  failure_time=CMA_Failure_times(theta,alpha,num_in_group)
+  inspection_initial=matrix(c(100,150,100,150,100,150,100,150),4,2,byrow=T)
+  num_matrix_initial=num_in_inspection_CMA(failure_time,inspection_initial)
+  fix_inspection=matrix(50+rep(50*(1:num_of_inspection),4),4,num_of_inspection,byrow = T)
+  fix_num_matrix=num_in_inspection_CMA(failure_time,fix_inspection)
+  para_fix=optim(c(3.5,9,1.9,0.17),Weibull_likelihood_interval_CMA,num_of_failure=fix_num_matrix,check_time=fix_inspection,method = "BFGS",hessian = T)
+  lowerlimit1=para_fix$par-1.96*sqrt(diag(solve(para_fix$hessian)))
+  upperlimit1=para_fix$par+1.96*sqrt(diag(solve(para_fix$hessian)))
+  para_fix=para_fix$par
+  para_est=optim(c(3.5,9,1.9,0.17),Weibull_likelihood_interval_CMA,num_of_failure=num_matrix_initial,check_time=inspection_initial,method = "BFGS")$par
+  theta_est=exp(para_est[2])*deltaT^(-para_est[3])*exp(para_est[4]/(K*Tmax))
+  inspection_time=rep(0,num_of_inspection)
+  for (i in 1:4) {
+    inspection=c(100,150,optim(c(200,250,300,350), TR_CMA_conditional,t_past=c(100,150),theta=theta_est[i],alpha=4,deltaT=deltaT[i],Tmax=Tmax[i],method = "BFGS")$par)
+    inspection_time=rbind(inspection_time,inspection)
+  }
+  inspection_time=inspection_time[-1,]
+  
+  already_inspection_time=matrix(0,4,num_of_inspection)
+  already_inspection_time[,c(1,2)]=matrix(c(100,150,100,150,100,150,100,150),4,2,byrow=T)
+  while (already_inspection_time[1,num_of_inspection]==0) {
+    position=nonzerominposition(inspection_time-already_inspection_time)
+    ### Next inspection
+    if(inspection_time[position]>max(already_inspection_time)){
+      already_inspection_time[position]=inspection_time[position]
+      ### For the spectial case that after a inspection, the next suggested time is even more earlier
+    }else{
+      already_inspection_time[position]=max(already_inspection_time)
+    }
+    num_matrix=num_in_inspection_CMA(failure_time,inspection_time)
+    para_est=optim(c(3.5,9,1.9,0.17),renew_Weibull_likelihood_interval_CMA,num_matrix=num_matrix,already_inspection_time=already_inspection_time,method = "BFGS")$par
+    theta_est=exp(para_est[2])*deltaT^(-para_est[3])*exp(para_est[4]/(K*Tmax))
+    
+    inspection_time=rep(0,num_of_inspection)
+    for (i in 1:4) {
+      n=sum(already_inspection_time[i,]!=0)
+      if(n!=num_of_inspection){
+        initial_inspection=already_inspection_time[i,n]+(1:(num_of_inspection-n))*50
+        inspection=c(already_inspection_time[i,1:n],optim(initial_inspection, TR_CMA_conditional,t_past=already_inspection_time[i,1:n],theta=theta_CMA[i],alpha=para_est[1],deltaT=deltaT[i],Tmax=Tmax[i],method = "BFGS")$par)
+      }
+      else{
+        inspection=already_inspection_time[i,1:n]
+      }
+      inspection_time=rbind(inspection_time,inspection)
+    }
+    inspection_time=inspection_time[-1,]
+  }
+  final_para=optim(c(4.5,8.8,1.77,0.17),renew_Weibull_likelihood_interval_CMA,num_matrix=num_matrix,already_inspection_time=already_inspection_time,method = "BFGS",hessian = T)
+  lowerlimit=final_para$par-1.96*sqrt(diag(solve(final_para$hessian)))
+  upperlimit=final_para$par+1.96*sqrt(diag(solve(final_para$hessian)))
+  tr=-TR_CMA(inspection_time[1,],theta_CMA[1],4,deltaT[1],Tmax[1])-TR_CMA(inspection_time[2,],theta_CMA[2],4,deltaT[2],Tmax[2])-TR_CMA(inspection_time[3,],theta_CMA[3],4,deltaT[3],Tmax[3])-TR_CMA(inspection_time[4,],theta_CMA[4],4,deltaT[4],Tmax[4])
+  return(list(inspection_time,num_matrix,fix_num_matrix,c(para_est,para_fix,tr),round(c(lowerlimit,upperlimit),4),round(c(lowerlimit1,upperlimit1),4)))
+}
+
+renew_Weibull_likelihood_interval_CMA=function(parameter,num_matrix,already_inspection_time){
+  alpha=parameter[1]
+  A=exp(parameter[2])
+  b=parameter[3]
+  Ea=parameter[4]
+  theta=A*deltaT^(-b)*exp(Ea/(K*Tmax))
+  m=dim(num_matrix)[1]
+  logL=0
+  for (i in 1:m) {
+    p=c()
+    F=0
+    n=sum(already_inspection_time[i,]!=0)
+    for (j in 1:n) {
+      p=c(p,pweibull(already_inspection_time[i,j],alpha,theta[i])-F)
+      F=pweibull(already_inspection_time[i,j],alpha,theta[i])
+    }
+    p=c(p,1-F)
+    num_of_censoring=num_in_group-sum(num_matrix[i,1:n])
+    logL=logL+sum(c(num_matrix[i,1:n],num_of_censoring)*log(p))
+  }
+  return(-logL)
+}
+
+##### Illustrative Example
+#2499(20)
+set.seed(2499)
+num_in_group=20; Ni=20
+CMA_trace_simulation(theta_CMA,4,deltaT,Tmax,num_in_group=20,6)
+
+#inspection  100  150 281.0735 339.9512 392.4031 439.0734
+#inspection  100  150 223.2738 267.9430 306.0326 344.8885
+#inspection  100  150 214.5208 252.5675 284.5697 319.9307
+#inspection  100  150 181.6986 214.5208 235.1073 260.6031
+### num matrix of RITO
+#[,1] [,2] [,3] [,4] [,5] [,6] [,7]
+#[1,]    0    0    4    3   11    1    1
+#[2,]    1    1    5    8    4    0    1
+#[3,]    2    3    9    3    1    2    0
+#[4,]    3    6    5    5    1    0    0
+### num matrix of fixed-time
+#[,1] [,2] [,3] [,4] [,5] [,6] [,7]
+#[1,]    0    0    0    3    3    6    8
+#[2,]    1    1    5    5    7    0    1
+#[3,]    2    3    8    4    2    1    0
+#[4,]    3    6    8    3    0    0    0
+
+### RITO CI             [3.3889,5.0457] [7.0394,14.3649] [1.8924,3.0902] [0.1453,0.2822]
+### Fixed-time Estimate [3.0679,4.6246] [6.8428,15.0928] [1.8460,3.2474] [0.1341,0.2925]
+###Relative Efficiency=1.131133e+06/2/577290=0.9796922
